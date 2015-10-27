77b87238c545f83146d2e2a43685bfdb2cc9970e
Brent Komer <brent.komer@gmail.com>
2015-10-27 14:40:57 -0400

    allowing the network used to be swapped easily

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -8,6 +8,15 @@ import getpass
 import sys
 from copy import deepcopy
 
+# Smush the 5 images together into one, otherwise treat them separately
+smush = True
+
+# Create the full confusion matrix, including sections not needed
+full = True
+
+# The type of pre-trained deep network to get the features from
+net_type = 'GoogLeNet' #'AlexNet'
+
 # Check the username, so the same code can work on all of our computers
 user = getpass.getuser()
 if user == 'ctnuser':
@@ -36,23 +45,36 @@ plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 index_mat = sio.loadmat(path_prefix + 'IndexToFilename.mat')['IndexToFilename'][0]
-training_start_index = 0
-training_end_index = 4804
-testing_start_index = 4804
-testing_end_index = len(index_mat)
+
+# MATLAB code uses 4789 as the split point, and this seems to match the data beter
+# The dataset itself claims 4804 is the split point, but this looks to be incorrect
+if full:
+  training_start_index = 0
+  training_end_index = len(index_mat)#4789 #4804
+  testing_start_index = 0#4789 #4804
+  testing_end_index = len(index_mat)
+else:
+  training_start_index = 0
+  training_end_index = 4789 #4804
+  testing_start_index = 4789 #4804
+  testing_end_index = len(index_mat)
 
 training_images = []
 testing_images = []
 
 # AlexNet can do a batch_size of 50
 # GoogLeNet needs a smaller batch_size, 10 works
-batch_size = 10 #50
+if net_type == 'GoogLeNet':
+  batch_size = 10
+elif net_type == 'AlexNet':
+  batch_size = 50
 
-# Smush the 5 images together into one, otherwise treat them separately
-smush = True
 
 # Which layer to get the features from
-layer = 'inception_4e/3x3' #'conv4'
+if net_type == 'GoogLeNet':
+  layer = 'inception_4c/3x3'
+elif net_type == 'AlexNet':
+  layer = 'conv4'
 
 if smush:
     # TODO: make sure concatenation is along the correct axis
@@ -92,9 +114,11 @@ transformer.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_
 transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
 transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
 
-# set net to batch size of 50
-#net.blobs['data'].reshape(batch_size,3,227,227) # AlexNet uses 227*227
-net.blobs['data'].reshape(batch_size,3,224,224) # GoogLeNet uses 224x224
+# set net batch size
+if net_type == 'GoogLeNet':
+  net.blobs['data'].reshape(batch_size,3,224,224) # GoogLeNet uses 224x224
+elif net_type == 'AlexNet':
+  net.blobs['data'].reshape(batch_size,3,227,227) # AlexNet uses 227*227
 
 # TODO: use something better than a list
 training_features = []
@@ -106,16 +130,16 @@ confusion_matrix = np.zeros((len(training_images), len(testing_images)))
 def vis_square(data, padsize=1, padval=0):
     data -= data.min()
     data /= data.max()
-    
+
     # force the number of filters to be square
     n = int(np.ceil(np.sqrt(data.shape[0])))
     padding = ((0, n ** 2 - data.shape[0]), (0, padsize), (0, padsize)) + ((0, 0),) * (data.ndim - 3)
     data = np.pad(data, padding, mode='constant', constant_values=(padval, padval))
-    
+
     # tile the filters into an image
     data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
     data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
-    
+
     plt.imshow(data)
     plt.figure()
     #plt.show()
@@ -207,20 +231,27 @@ if extra != 0:
       confusion_matrix[i,j] = np.linalg.norm(feat - training_features[i])
     j += 1
 
+# Optional plotting of features
 #for i in range(len(training_images)):
 #  vis_square(training_features[i], padval=0.5)
 #plt.show()
 
 print( confusion_matrix )
-#print( "Saving Confusion Matrix to Pickle File..." )
-#pickle.dump(confusion_matrix, open('test_confusion_matrix_smush.p','wb'))
-#print( "Saving Complete!" )
 
 # Remove any slashes from layer name
 layer = layer.replace('/','-')
 
+# Construct file name
+fname = 'conf_mat'
+if smush:
+  fname += '_smush'
+if full:
+  fname += '_full'
+fname += '_' + net_type.lower() + '_' + layer + '.h5'
+
+# Save to HDF5 format
 print( "Saving Confusion Matrix for %s to HDF5 File..." % layer )
-h5f = h5py.File('conf_mat_smush_googlenet_%s.h5' % layer, 'w')
+h5f = h5py.File(fname, 'w')
 h5f.create_dataset('dataset', data=confusion_matrix)
 h5f.close()
 print( "Saving Complete!" )
