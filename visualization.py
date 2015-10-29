14a8aba8cb435c986b963123265b5762941f1362
Brent Komer <brent.komer@gmail.com>
2015-10-29 16:40:04 -0400

    option for viewing specific layer

--- visualization.py
+++ visualization.py
@@ -8,9 +8,13 @@ import sys
 fname = 'conf_mat_smush_inception_4e-3x3.h5'
 dname = 'dataset'
 
-if len(sys.argv) == 2:
+if len(sys.argv) >= 2:
   fname = sys.argv[1]
   dname = 'dataset'
+  layer = 10
+
+if len(sys.argv) == 3:
+  layer = int(sys.argv[2])
 
 h5f = h5py.File(fname, 'r')
 data = h5f[dname][:]
@@ -18,5 +22,9 @@ h5f.close()
 
 print(data)
 
-plt.imshow(data)
+# If multiple confusion matrices are saved, only display a specific layer
+if len(data.shape) == 3:
+  plt.imshow(data[layer,...])
+else:
+  plt.imshow(data)
 plt.show()
